5900cc75b59700e3ab67349c4a562c05a356f89a
cuswan022 <cuswan022@student.wethinkcode.co.za>
2023-04-27 14:46:17 +0200

    DropDown not working but is the code from the website

---
+++ DropDownBox/ButtonGrid/main.py
@@ -0,0 +1,3 @@
+from kivy.uix.button import Button
+from kivy.uix.stacklayout import StackLayout
+
--- DropDownBox/DropDown/main.py
+++ DropDownBox/DropDown/main.py
@@ -1,3 +1,4 @@
+import kivy
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
 from kivy.base import runTouchApp
@@ -7,12 +8,15 @@ from kivy.base import runTouchApp
 dropdown = DropDown()
 list_of_environments = ["Tavern", "Dungeon", "Swamp"]
 for i in list_of_environments:
+
     """makes a button widget with a height of 44 and 
     sets it text to current iterator.size_hint-y set to None so dropdown can choose its own height"""
     btn = Button(text=i, size_hint_y=None, height=44)
+
     """binds what the button does IE. on release it sets the
      dropdown boxes text to whatever button was pressed"""
     btn.bind(on_release=lambda button: dropdown.select(btn.text))
+
     """adding each button to the dropdown"""
     dropdown.add_widget(btn)
 
@@ -20,6 +24,7 @@ main_button = Button(text=" Environments ", size_hint=(None, None))
 
 """when the mainbutton is pressed it will open the dropdown"""
 main_button.bind(on_select=dropdown.open)
+
 """Changes the mainbutton's text to whatever button in the dropdown was pressed"""
 dropdown.bind(on_select=lambda instance, x: setattr(main_button, 'text', x))
 
