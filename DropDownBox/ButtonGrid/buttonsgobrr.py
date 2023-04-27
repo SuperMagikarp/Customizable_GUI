f1b29fdd2deaedafdcb936097c1a61473090df80
cuswan022 <cuswan022@student.wethinkcode.co.za>
2023-04-27 16:31:25 +0200

    New Button Layout testing

--- DropDownBox/ButtonGrid/buttonsgobrr.py
+++ DropDownBox/ButtonGrid/buttonsgobrr.py
@@ -5,12 +5,30 @@ from kivy.app import App
 
 class MyApp(App):
 
+    dictionary_of_envs = {"Forest": "Trees",
+                          "River": "fish",
+                          "Dungeon": "Skeleton",
+                          "Market": "coins"}
+
+    def use_the_stuff(self, environment):
+        if environment == "Forest":
+            print(f"there are {self.dictionary_of_envs[environment]} here")
+        elif environment == "Dungeon":
+            print(f"there are {self.dictionary_of_envs[environment]} here")
+        elif environment == "Market":
+            print(f"there are {self.dictionary_of_envs[environment]} here")
+        elif environment == "River":
+            print(f"there are {self.dictionary_of_envs[environment]} here")
+
     def build(self):
-        list_of_environments = ["Tavern", "Dungeon", "Swamp"]
-        layout = GridLayout()
-        for i in list_of_environments:
-            layout.add_widget(Button(text=i))
+        layout = GridLayout(cols=1)
+        for i in self.dictionary_of_envs:
+            btn = Button(text=i)
+            layout.add_widget(btn)
+            btn.bind(on_release=lambda x: self.use_the_stuff(x.text))
         return layout
+
+
 
 
 if __name__ == "__main__":
--- DropDownBox/DropDown/main.py
+++ DropDownBox/DropDown/main.py
@@ -1,35 +1,42 @@
-import kivy
+
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
 from kivy.base import runTouchApp
-
+from kivy.uix.gridlayout import GridLayout
 
 """creating a dropdown object"""
-dropdown = DropDown()
 list_of_environments = ["Tavern", "Dungeon", "Swamp"]
-for i in list_of_environments:
-
-    """makes a button widget with a height of 44 and 
-    sets it text to current iterator.size_hint-y set to None so dropdown can choose its own height"""
-    btn = Button(text=i, size_hint_y=None, height=44)
-
-    """binds what the button does IE. on release it sets the
-     dropdown boxes text to whatever button was pressed"""
-    btn.bind(on_release=lambda button: dropdown.select(btn.text))
-
-    """adding each button to the dropdown"""
-    dropdown.add_widget(btn)
-
-main_button = Button(text=" Environments ", size_hint=(None, None))
-
-"""when the mainbutton is pressed it will open the dropdown"""
-main_button.bind(on_select=dropdown.open)
-
-"""Changes the mainbutton's text to whatever button in the dropdown was pressed"""
-dropdown.bind(on_select=lambda instance, x: setattr(main_button, 'text', x))
-
-runTouchApp(main_button)
+list_of_dice = ["d2", "d4", "d6", "d8"]
+layout = GridLayout(cols=2)
 
 
+def dropdown_maker(listofbuttons):
+    dropdown = DropDown()
+    for i in range(len(listofbuttons)):
+        """makes a button widget with a height of 44 and 
+        sets it text to current iterator.size_hint-y set to None so dropdown can choose its own height"""
+        btn = Button(text=listofbuttons[i], size_hint_y=None, height=44)
+
+        """binds what the button does IE. on release it sets the
+         dropdown boxes text to whatever button was pressed"""
+        btn.bind(on_release=lambda button: dropdown.select(listofbuttons.index(button.text)))
+
+        """adding each button to the dropdown"""
+        dropdown.add_widget(btn)
+    main_button = Button(text=" Environments ", size_hint=(None, None))
+
+    """when the mainbutton is pressed it will open the dropdown"""
+    main_button.bind(on_release=dropdown.open)
+
+    """Changes the mainbutton's text to whatever button in the dropdown was pressed"""
+    dropdown.bind(on_select=lambda instance, x: print(x))
+    return main_button
 
 
+main_button1 = dropdown_maker(list_of_environments)
+main_button2 = dropdown_maker(list_of_dice)
+
+layout.add_widget(main_button1)
+layout.add_widget(main_button2)
+
+runTouchApp(layout)
