5900842c446000e436904e0afb95b843be8114a5
riecenaidoo <riecenaidoo@outlook.com>
2023-05-09 19:51:19 +0200

    OOP Implementation of the Dice Roller GUI

--- main.py
+++ main.py
@@ -4,100 +4,66 @@ from kivy.app import App
 
 import random
 
-"""
-{
-    "d2" : DiceButton(2)
-}
 
-class DiceButton{
-    
-    Random rand_seed
+class DiceButton:
 
-    Button increase;
-    Button decrease;
-    Button dice;
-    
-    int dice_to_roll;
-    int diceSize;
-    
-    
-    void increase(){
-        dice_to_roll++;
-    }
-    
-    void decrease(){
-        if(dice_to_roll>0){
-            dice_to_roll--;
-        }
-    }
-    
-    String DoRoll(){
-        
-        roll_result = []
-        sum = 0
+    def __init__(self, dice_size):
+        self.dice_to_roll = 1
+        self.dice_size = dice_size
 
-        for(dice in range(dice_to_roll)){
-            dice_result = random.rand(dice_size)
-            roll_result.append(dice_result)
-            sum += roll_result
-        }
-    
-        f"Result : {sum} = {dice_to_roll}d{dice_size} {roll_result}
-    
-    }
-}
+        self.increase_button = Button(text="+", size_hint_x=0.25)
+        self.decrease_button = Button(text="-", size_hint_x=0.25)
+        self.roll_button = Button(text=f"{self.dice_to_roll}d{self.dice_size}", size_hint_x=0.5)
 
+        self.increase_button.bind(on_release=lambda x: self.do_increase())
+        self.decrease_button.bind(on_release=lambda x: self.do_decrease())
+        self.roll_button.bind(on_release=lambda x: self.do_roll())
 
-"""
+    def update_label(self):
+        self.roll_button.text = f"{self.dice_to_roll}d{self.dice_size}"
+
+    def do_increase(self):
+        self.dice_to_roll += 1
+        self.update_label()
+
+    def do_decrease(self):
+        if self.dice_to_roll > 1:
+            self.dice_to_roll -= 1
+            self.update_label()
+
+    def do_roll(self):
+        roll_result_sum = 0
+        dice_rolled_results = []
+
+        for i in range(self.dice_to_roll):
+            rolled_dice = (random.randrange(self.dice_size)) + 1
+            dice_rolled_results.append(rolled_dice)
+            roll_result_sum += rolled_dice
+
+        # Send to Webhook
+        print(f"Result : {roll_result_sum} = {self.dice_to_roll}d{self.dice_size} {dice_rolled_results}")
 
 
 class MyApp(App):
-    dice_to_buttons = {}
-
-    amount_of_dice = {
-        "d2": [1, ""],
-        "d4": [1, ""],
-        "d6": [1, ""],
-        "d8": [1, ""],
-        "d10": [1, ""],
-        "d12": [1, ""],
-        "d20": [1, ""],
-        "d100": [1, ""],
-    }
-
-    def increase_dice(self, button):
-        self.amount_of_dice[self.dice_to_buttons[button]][0] += 1
-
-    def decrease_dice(self, button):
-        self.amount_of_dice[self.dice_to_buttons[button]][0] -= 1
-
-    def roll_the_dice(self, dice_type):
-        d = int(dice_type.split("d")[1])
-        print(str(self.amount_of_dice[dice_type][0]) + dice_type + "'s have been rolled")
-        total = 0
-        for i in range(self.amount_of_dice[dice_type][0]):
-            result = random.randint(1, d)
-            total += result
-        print("Result: " + str(total))
-
     def build(self):
-        cols = 3
-        grid = GridLayout(cols=cols)
-        for j in self.amount_of_dice:
-            for i in range(cols):
-                if i == 0:
-                    button = Button(text="-", size_hint_x=0.25)
-                    button.bind(on_release=lambda x: self.decrease_dice(x))
-                    self.dice_to_buttons[button] = j
-                elif i == cols - 1:
-                    button = Button(text="+", size_hint_x=0.25)
-                    button.bind(on_release=lambda x: self.increase_dice(x))
-                    self.dice_to_buttons[button] = j
-                else:
-                    button = Button(text=j, size_hint_x=0.5)
-                    button.bind(on_release=lambda x: self.roll_the_dice(x.text))
-                    self.amount_of_dice[j][1] = button
-                grid.add_widget(button)
+        dice_buttons = [
+            DiceButton(2),
+            DiceButton(4),
+            DiceButton(6),
+            DiceButton(8),
+            DiceButton(10),
+            DiceButton(12),
+            DiceButton(20),
+            DiceButton(100),
+        ]
+
+        grid = GridLayout(cols=3)  # [-] [dX] [+]
+
+        for dice in dice_buttons:
+            grid.add_widget(dice.decrease_button)
+            grid.add_widget(dice.roll_button)
+            grid.add_widget(dice.increase_button)
+
         return grid
 
 
